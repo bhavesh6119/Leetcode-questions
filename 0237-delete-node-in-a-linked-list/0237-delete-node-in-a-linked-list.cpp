2a921760ffe306b1227439f05035f04ef11d35cd
bhavesharora <143539122+bhavesh6119@users.noreply.github.com>
2026-06-25 11:49:17 +0530

    Time: 10 ms (37.05%), Space: 12.4 MB (22.03%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
@@ -9,9 +9,10 @@
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-        ListNode *temp=node->next;
-        node->val=temp->val;
-        node->next=temp->next;
-        delete temp;
+        ListNode *temp=node->next;//we created a temp node and added the value of next node to it
+        node->val=temp->val;//then copied that value in current node
+        node->next=temp->next;//then copied the address it contains 
+        delete temp;//delted the temp node 
+        //now the curr data is been replaced by the next one and next oe is deleted
     }
 };
