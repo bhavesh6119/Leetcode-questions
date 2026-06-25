c39d852d398dcf3243a36e608329172c7f54ab68
bhavesharora <143539122+bhavesh6119@users.noreply.github.com>
2026-06-25 16:22:00 +0530

    Time: 0 ms (100%), Space: 13.4 MB (71.64%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -11,22 +11,22 @@
 class Solution {
 public:
     ListNode *reverse(ListNode *curr,ListNode *prev){
-        if(curr==NULL){
+        if(curr==NULL){//when curr becomes null means it is out bound then only prev remains so we return the prev
             return prev;
         }
-        ListNode *fut=curr->next;
-        curr->next=prev;
-        return reverse(fut,curr);
+        ListNode *fut=curr->next;//storing the next node in fut
+        curr->next=prev;//adding the address of the prev to the curr->next
+        return reverse(fut,curr);//passing the new curr and prev
     }
     ListNode* reverseList(ListNode* head) {
         // //1st approach
         // vector<int>ans;
         // ListNode *temp=head;
         // while(temp!=NULL){
-        //     ans.push_back(temp->val);
+        //     ans.push_back(temp->val);//here we are traversing the list and storing the values onto the ans array
         //     temp=temp->next;
         // }
-        // int i=ans.size()-1;
+        // int i=ans.size()-1;//then traversing from last index of the ans array to get the values and replace them in the list
         // temp=head;
         // while(temp!=NULL){
         //     temp->val=ans[i];
@@ -35,14 +35,15 @@ public:
         // }
 
         // // 2nd approach
+        //here we have created 3 pointers and while traversing upadting them side by side
         // ListNode *curr=head, *prev=NULL, *fut=NULL;
         // while(curr!=NULL){
-        //     fut=curr->next;
-        //     curr->next=prev;
-        //     prev=curr;
-        //     curr=fut;
+        //     fut=curr->next;//adding the next node to fut
+        //     curr->next=prev;//adding the prev address to curr
+        //     prev=curr;//making curr the prev
+        //     curr=fut;//updating curr as fut
         // }
-        // head=prev;
+        // head=prev;//at last the remaining is the prev so add it into head
 
         // 3rd approach Recursion
         head=reverse(head,NULL);
