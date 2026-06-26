63fa6d9d525eff9eab8ee8edab930b0e37fb439a
bhavesharora <143539122+bhavesh6119@users.noreply.github.com>
2026-06-26 15:19:18 +0530

    Time: 0 ms (100%), Space: 16.5 MB (31.3%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -11,33 +11,58 @@
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        // 1st approach
-        if(head==NULL || head->next==NULL){//edge case if only one node is present
+        // // 1st approach
+        // if(head==NULL || head->next==NULL){//edge case if only one node is present
+        //     return head;
+        // }
+        // int count=0;
+        // ListNode *temp=head;
+        // while(temp!=NULL){
+        //     count++;//counting the number of nodes
+        //     temp=temp->next;
+        // }
+        // k=k%count;//taking the exact number of rotations
+        // if(k==0){//if 0 no rotation to be made
+        //     return head;
+        // }
+        // count-=k;//reducing count by k so that we can go to the exact node where we have to break the list
+        // ListNode *curr=head, *prev=NULL;
+        // while(count--){
+        //     prev=curr;
+        //     curr=curr->next;
+        // }
+        // prev->next=NULL;//here list breaks 
+        // ListNode *tail=curr;
+        // while(tail->next!=NULL){//raversing on the other half of the list
+        //     tail=tail->next;
+        // }
+        // tail->next=head;//connecting it to main list
+        // head=curr;//making the new head
+        // return head;
+
+        //second approach
+        ListNode *temp=head;
+        if(head==NULL || head->next==NULL){
             return head;
         }
-        int count=0;
-        ListNode *temp=head;
-        while(temp!=NULL){
-            count++;//counting the number of nodes
+        vector<int>ans;
+        while(temp!=NULL){//here we are inserting the node values into an array
+            ans.push_back(temp->val);
             temp=temp->next;
         }
-        k=k%count;//taking the exact number of rotations
-        if(k==0){//if 0 no rotation to be made
-            return head;
+        k=k%ans.size();//getting the eaxct rotations
+        //rotating the array
+        reverse(ans.begin(),ans.end());
+        reverse(ans.begin(),ans.begin()+k);
+        reverse(ans.begin()+k,ans.end());
+        temp=head;
+        int i=0;
+        //again replacing the values of the nodes with values of rotated array
+        while(temp!=NULL){
+            temp->val=ans[i];
+            i++;
+            temp=temp->next;
         }
-        count-=k;//reducing count by k so that we can go to the exact node where we have to break the list
-        ListNode *curr=head, *prev=NULL;
-        while(count--){
-            prev=curr;
-            curr=curr->next;
-        }
-        prev->next=NULL;//here list breaks 
-        ListNode *tail=curr;
-        while(tail->next!=NULL){//raversing on the other half of the list
-            tail=tail->next;
-        }
-        tail->next=head;//connecting it to main list
-        head=curr;//making the new head
         return head;
     }
 };
