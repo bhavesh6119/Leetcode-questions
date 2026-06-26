f894be24ab5e4857c7750f320081205f3891caa4
bhavesharora <143539122+bhavesh6119@users.noreply.github.com>
2026-06-26 14:00:25 +0530

    Time: 52 ms (32.25%), Space: 39.6 MB (57.58%) - LeetHub

--- 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
+++ 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int countMajoritySubarrays(vector<int>& nums, int target) {
         int n=nums.size();
-        int total=0;
-       for(int i=0;i<n;i++){
-        int targetcount=0;
-        int othercount=0;
+        int total=0;//keeping the track of the subarrrays that has majority element as target
+       for(int i=0;i<n;i++){//running an outer loop
+        int targetcount=0;//maintaining the target count at each step 
+        int othercount=0;//same with non target element
         for(int j=i;j<n;j++){
-            if(nums[j]==target){
+            if(nums[j]==target){//checking if it is the target element or not
                 targetcount++;
             }else{
                 othercount++;
             }
-            if(targetcount>othercount){
+            if(targetcount>othercount){//comparing if it is greater we will increase our total that means we got our subarray with majority element as target
                 total++;
             }
         }
