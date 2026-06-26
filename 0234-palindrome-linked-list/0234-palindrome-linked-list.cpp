17ae825b9c9c2a84862d7d5cf4abef5849fa3eaa
bhavesharora <143539122+bhavesh6119@users.noreply.github.com>
2026-06-26 15:43:27 +0530

    Time: 4 ms (50.19%), Space: 132.1 MB (8.13%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,7 +10,7 @@
  */
 class Solution {
 public:
-    ListNode *reverse(ListNode *curr, ListNode *prev){
+    ListNode *reverse(ListNode *curr, ListNode *prev){//recursion for reversing the other half of the list
         if(curr==NULL){
             return prev;
         }
@@ -19,31 +19,51 @@ public:
         return reverse(fut,curr);
     }
     bool isPalindrome(ListNode* head) {
-        // first approach
-        if(head->next==NULL){
-            return true;
-        }
+        // // first approach
+        // if(head->next==NULL){//edge case if only one node is present
+        //     return true;
+        // }
+        // ListNode *temp=head;
+        // int count=0;
+        // while(temp!=NULL){
+        //     count++;//counting the number of nodes
+        //     temp=temp->next;
+        // }
+        // count/=2;//dividing it by 2 so that break the list in half
+        // ListNode *curr=head, *prev=NULL;
+        // while(count--){//for the first half 
+        //     prev=curr;
+        //     curr=curr->next;
+        // }
+        // prev->next=NULL;
+        // curr=reverse(curr,NULL);//for second half reversing it to check palindrome
+        // //just normal algo for checking palindrome
+        // ListNode *head1=head, *head2=curr;
+        // while(head1){
+        //     if(head1->val != head2->val){
+        //         return false;
+        //     }
+        //     head1=head1->next;
+        //     head2=head2->next;
+        // }
+        // return true;
+
+        //second approach
+        vector<int>ans;
         ListNode *temp=head;
-        int count=0;
-        while(temp!=NULL){
-            count++;
+        while(temp!=NULL){//just adding the values of nodes to an array
+            ans.push_back(temp->val);
             temp=temp->next;
         }
-        count/=2;
-        ListNode *curr=head, *prev=NULL;
-        while(count--){
-            prev=curr;
-            curr=curr->next;
-        }
-        prev->next=NULL;
-        curr=reverse(curr,NULL);
-        ListNode *head1=head, *head2=curr;
-        while(head1){
-            if(head1->val != head2->val){
+        //applying two pointers approach to check palindrome in array
+        int start=0;
+        int end=ans.size()-1;
+        while(start<=end){
+            if(ans[start]!=ans[end]){
                 return false;
             }
-            head1=head1->next;
-            head2=head2->next;
+            start++;
+            end--;
         }
         return true;
     }
