a84e42646609a0e2c432067d3caf15960d686ce0
bhavesharora <143539122+bhavesh6119@users.noreply.github.com>
2026-06-26 15:10:57 +0530

    Time: 0 ms (100%), Space: 16.5 MB (31.3%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -12,32 +12,32 @@ class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
         // 1st approach
-        if(head==NULL || head->next==NULL){
+        if(head==NULL || head->next==NULL){//edge case if only one node is present
             return head;
         }
         int count=0;
         ListNode *temp=head;
         while(temp!=NULL){
-            count++;
+            count++;//counting the number of nodes
             temp=temp->next;
         }
-        k=k%count;
-        if(k==0){
+        k=k%count;//taking the exact number of rotations
+        if(k==0){//if 0 no rotation to be made
             return head;
         }
-        count-=k;
+        count-=k;//reducing count by k so that we can go to the exact node where we have to break the list
         ListNode *curr=head, *prev=NULL;
         while(count--){
             prev=curr;
             curr=curr->next;
         }
-        prev->next=NULL;
+        prev->next=NULL;//here list breaks 
         ListNode *tail=curr;
-        while(tail->next!=NULL){
+        while(tail->next!=NULL){//raversing on the other half of the list
             tail=tail->next;
         }
-        tail->next=head;
-        head=curr;
+        tail->next=head;//connecting it to main list
+        head=curr;//making the new head
         return head;
     }
 };
