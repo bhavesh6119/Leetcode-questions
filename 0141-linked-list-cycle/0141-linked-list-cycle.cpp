a90a141631eca31a0c4e1703d31e874f215de361
bhavesharora <143539122+bhavesh6119@users.noreply.github.com>
2026-06-29 20:53:13 +0530

    Time: 9 ms (55.35%), Space: 11.9 MB (22.89%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,15 +9,28 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        unordered_map<ListNode*,bool>visited;//making a map to store the adresses and marking them marked as we traverse
-        ListNode *curr=head;
-        while(curr!=NULL){
-            if(visited[curr]==1){//if the adrress is already marked it means it is a loop
-                return true;
+        // // 1st approach
+        // unordered_map<ListNode*,bool>visited;//making a map to store the adresses and marking them marked as we traverse
+        // ListNode *curr=head;
+        // while(curr!=NULL){
+        //     if(visited[curr]==1){//if the adrress is already marked it means it is a loop
+        //         return true;
+        //     }
+        //     visited[curr]=1;//make them marked
+        //     curr=curr->next;
+        // }
+        // return false;
+
+        // 2nd approach
+        ListNode *slow=head;
+        ListNode *fast=head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast){//if they reach at same pos means they are in a loop
+                return 1;
             }
-            visited[curr]=1;//make them marked
-            curr=curr->next;
         }
-        return false;
+        return 0;
     }
 };
