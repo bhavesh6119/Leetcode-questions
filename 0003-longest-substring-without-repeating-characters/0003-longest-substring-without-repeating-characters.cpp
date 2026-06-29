09cfdfb7ab4211deac40d0665f6c8681daf7f830
bhavesharora <143539122+bhavesh6119@users.noreply.github.com>
2026-06-29 11:21:31 +0530

    Time: 6 ms (66.47%), Space: 10.4 MB (97.5%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -5,15 +5,15 @@ public:
         int first=0;
         int second=0;
         int length=0;
-        while(second<s.size()){
-            while(arr[s[second]]){
+        while(second<s.size()){//travesring till the string size
+            while(arr[s[second]]){//if the string has been marked and is positive then reduce all the string befor it till it and unmark it
                 arr[s[first]]=0;
                 first++;
             }
-            arr[s[second]]=1;
-            length=max(length,second-first+1);
+            arr[s[second]]=1;//now mark that char again and begin a new substring 
+            length=max(length,second-first+1);//calculating length at every substring
             second++;
         }
-        return length;
+        return length;//at last the length that remained
     }
 };
