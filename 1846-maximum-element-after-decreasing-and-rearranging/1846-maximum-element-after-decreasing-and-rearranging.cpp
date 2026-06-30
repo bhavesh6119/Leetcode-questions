0be77c0975ee7552f59aa973d0f4e9d8616d8083
bhavesharora <143539122+bhavesh6119@users.noreply.github.com>
2026-06-30 12:37:09 +0530

    Time: 0 ms (100%), Space: 55.2 MB (49.34%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
@@ -2,16 +2,30 @@ class Solution {
 public:
     int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
         int n =arr.size();
-        vector<int> freq(n+1,0);//creating a freq array 
-        for(int num : arr){
-            ++freq[min(num,n)];//storing the freq of elements at pos of min of the element or the n 
-            //as n can be the highest max value that can in the array 
+        //1st approach
+        // vector<int> freq(n+1,0);//creating a freq array 
+        // for(int num : arr){
+        //     ++freq[min(num,n)];//storing the freq of elements at pos of min of the element or the n 
+        //     //as n can be the highest max value that can in the array 
+        // }
+        // int ans=1;//initialsing answer with 1
+        // for(int num=2;num<=n;num++){//now traversing from 2 to n
+        //     ans=min(ans+freq[num],num);//adding the freq of 2 to n to ans and replacing it with minimum of the added value or the curr num value 
+        //     //so from here we can get the max ans
+        // }
+        // return ans;
+
+        //second approach
+        sort(arr.begin(),arr.end());//sorting the array
+        int maxEl=-1;//initialising max with -1
+        for(int i=0;i<n;i++){//traversing the arr
+            if(i==0){
+                arr[0]=1;
+            }else if(abs(arr[i]-arr[i-1])>1){//if it satisfies
+                arr[i]=arr[i-1]+1;//then we just replace it with this
+            }
+                maxEl=max(maxEl,arr[i]);//update the max                
         }
-        int ans=1;//initialsing answer with 1
-        for(int num=2;num<=n;num++){//now traversing from 2 to n
-            ans=min(ans+freq[num],num);//adding the freq of 2 to n to ans and replacing it with minimum of the added value or the curr num value 
-            //so from here we can get the max ans
-        }
-        return ans;
+        return maxEl;
     }
 };
