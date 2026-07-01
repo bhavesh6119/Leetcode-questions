b8642a8f7a512152964a4dec591bd8a33c67979c
bhavesharora <143539122+bhavesh6119@users.noreply.github.com>
2026-07-01 19:59:08 +0530

    Time: 0 ms (100%), Space: 16.5 MB (70.33%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -13,34 +13,34 @@ public:
     ListNode* reverseKGroup(ListNode* head, int k) {
         ListNode*curr=head;
         int count=0;
-        while(curr){
+        while(curr){//we have first counted the no. of nodes
             count++;
             curr=curr->next;
         }
-        int n=count/k;
-        ListNode *first=new ListNode(0);
+        int n=count/k;//then divided it by the k as the nodes reversed should be a multiple of k
+        ListNode *first=new ListNode(0);//initialised a LL with a dummy node
         first->next=head;
         head=first;
-        while(first->next && n){
+        while(first->next && n){//till first->next!=NULL or till n exist
             int x=k;
             ListNode *second=first->next;
             ListNode *prev=first;
             ListNode *curr=first->next;
-            while(x && curr){
+            while(x && curr){//while they exist reverse the list
                 ListNode *front=curr->next;
                 curr->next=prev;
                 prev=curr;
                 curr=front;
                 x--;
             }
-            first->next=prev;
-            second->next=curr;
-            first=second;
-            n--;
+            first->next=prev;//replace the head 
+            second->next=curr;//replace the end
+            first=second;//move onto sesond group
+            n--;//reducing the n 
         }
         first=head;
         head=head->next;
-        delete first;
+        delete first;//removing dummy node
         return head;
     }
 };
