52aec6588f2d6c5e3ae1586b772488ed7e334ec7
bhavesharora <143539122+bhavesh6119@users.noreply.github.com>
2026-07-01 14:22:59 +0530

    Time: 0 ms (100%), Space: 15.6 MB (72.66%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
@@ -11,22 +11,22 @@
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-      ListNode*temp=new ListNode(0);
+      ListNode*temp=new ListNode(0);//creating a new list
       temp->next=head;
-      ListNode*prev=temp;
-      ListNode*curr=head;
+      ListNode*prev=temp;//pointing at temp node
+      ListNode*curr=head;//pointing at head node
       while(curr){
-        while(curr->next && curr->val == curr->next->val){
-            curr=curr->next;
+        while(curr->next && curr->val == curr->next->val){//if curr->next exist check if the value of curr and curr->next are same if yes then increment curr
+            curr=curr->next;//it reaches the last node which contains the duplicate
         }
 
-        if(prev->next==curr){
-            prev=prev->next;
-        }else{
+        if(prev->next==curr){//if prev->next==curr means curr has not moves ahead so it means it is not a duplicate value 
+            prev=prev->next;//so keep it and proceed to next node
+        }else{//else if they are not equal then the curr has moved ahead so curr->next has unique value use it after the prev
             prev->next=curr->next;
         }
-        curr=curr->next;
+        curr=curr->next;//increment curr
       }
-      return temp->next;
+      return temp->next;//return the next node of dummy node
     }
 };
