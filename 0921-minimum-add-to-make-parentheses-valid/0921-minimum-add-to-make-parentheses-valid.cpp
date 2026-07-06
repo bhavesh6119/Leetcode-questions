0fc3a1a2bc5463d7cdf1dc5093a16011464cb921
bhavesharora <143539122+bhavesh6119@users.noreply.github.com>
2026-07-06 12:20:40 +0530

    Time: 0 ms (100%), Space: 8.4 MB (82%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -1,19 +1,38 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-        stack<char>st;
+        // //TC=O(n) and SC=O(n) due to stack
+        // stack<char>st;
+        // int count=0;
+        // for(int i=0;i<s.size();i++){//iterating 
+        //     if(s[i]=='('){//if there is left present push it to the stack
+        //         st.push(s[i]);
+        //     }else{//if right appears
+        //         if(st.empty()){//if satck is empty then its not a pair increse the count of left to be added 
+        //             count++;
+        //         }else{//if left is present pair is valid then pop from stack
+        //             st.pop();
+        //         }
+        //     }
+        // }
+        // return count+st.size();//as there might be some left parenthesis in the stack that needs right one so its the total parenthesis required to make the string valid
+        
+
+        //optimised approachh 
+        //SC=O(1);
+        int left=0;
         int count=0;
-        for(int i=0;i<s.size();i++){//iterating 
-            if(s[i]=='('){//if there is left present push it to the stack
-                st.push(s[i]);
-            }else{//if right appears
-                if(st.empty()){//if satck is empty then its not a pair increse the count of left to be added 
-                    count++;
-                }else{//if left is present pair is valid then pop from stack
-                    st.pop();
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
+                left++;//here we are just maintaining the count of left parenthesis as pushing onto the stack takes space
+            }else{
+                if(left==0){//if it is 0 that means stack is empty
+                    count++;//increase the count of left required
+                }else{
+                    left--;//otherwise pop it out or reduce it if there is a pair
                 }
             }
         }
-        return count+st.size();//as there might be some left parenthesis in the stack that needs right one so its the total parenthesis required to make the string valid
+        return left+count;//total required parenthesis
     }
 };
