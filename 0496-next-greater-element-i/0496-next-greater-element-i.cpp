7cef906d870a3066380708e1142fe514eff0a9e7
bhavesharora <143539122+bhavesh6119@users.noreply.github.com>
2026-07-07 12:26:41 +0530

    Time: 3 ms (40.81%), Space: 12.1 MB (97.24%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        //brute force solution
         int n=nums1.size();
         int m=nums2.size();
-        vector<int>ans(n,-1);
-        int k=0;
-        while(k<n){
-         for(int i=0;i<m;i++){
-            if(nums1[k]==nums2[i]){
-                for(int j=i+1;j<m;j++){
+        vector<int>ans(n,-1);//make an array of size of num1 
+        int k=0;//for traversing on num1
+        while(k<n){//whike we can traverse
+         for(int i=0;i<m;i++){//traverse on the main array 
+            if(nums1[k]==nums2[i]){//check if it is equal or not 
+                for(int j=i+1;j<m;j++){//if found then search from the right of the current index
                     if(nums2[j]>nums2[i]){
-                        ans[k]=nums2[j];
-                        break;
+                        ans[k]=nums2[j];//if found the greater element just add it to ans array
+                        break;//if greater element found
                     }
                 }
-                   break;
+                 break;//if the element is matched no need to search further in the nums2 array
             }
          }
-         k++;
+         k++;//increment for array nums1
         }
-        return ans;
+        return ans;//result
     }
 };
