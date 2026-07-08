532d14e12fa0eb5d2f0c19efdd390e2595c710af
bhavesharora <143539122+bhavesh6119@users.noreply.github.com>
2026-07-08 17:34:35 +0530

    Time: 26 ms (41.02%), Space: 89.4 MB (10.79%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -2,35 +2,39 @@ class Solution {
 public:
     int largestRectangleArea(vector<int>& h) {
         int n=h.size();
-        vector<int>right(n,0);
-        vector<int>left(n,0);
+        vector<int>right(n,0);//right array to maintain the right smallest
+        vector<int>left(n,0);//left array to maintain the left smallest
         stack<int>st;
-        int ans=0;
-        for(int i=0;i<n;i++){
+        int ans=0;//for maintaining the area
+        for(int i=0;i<n;i++){//first traversing left to right
+        //finding the right next right smallest at every index
             while(!st.empty() && h[st.top()]>h[i]){
                 right[st.top()]=i;
                 st.pop();
             }
             st.push(i);
         }
+        //this we will do if we find no smallest
         while(!st.empty()){
-            right[st.top()]=n;
+            right[st.top()]=n;//fill it with n
             st.pop();
         }
-        for(int i=n-1;i>=0;i--){
+        for(int i=n-1;i>=0;i--){//traversing from right to left 
+        //finding the next left smallest for every index    
             while(!st.empty() && h[st.top()]>h[i]){
                 left[st.top()]=i;
                 st.pop();
             }
             st.push(i);
         }
+        //this we will do if we found no amllest on left 
         while(!st.empty()){
-            left[st.top()]=-1;
+            left[st.top()]=-1;//fill it with -1
             st.pop();
         }
 
-        for(int i=0;i<n;i++){
-            ans=max(ans,h[i]*(right[i]-left[i]-1));
+        for(int i=0;i<n;i++){//lastly traverse the arrays together and use that value to calculate the area 
+            ans=max(ans,h[i]*(right[i]-left[i]-1));//we have to take the max area
         }
         return ans;
     }
