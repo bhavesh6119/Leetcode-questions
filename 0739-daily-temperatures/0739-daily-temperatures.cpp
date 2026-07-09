e3f58df5592d2454005915f5767e768612565ea6
bhavesharora <143539122+bhavesh6119@users.noreply.github.com>
2026-07-09 21:10:40 +0530

    Time: 26 ms (37.07%), Space: 102.8 MB (91.59%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -14,14 +14,18 @@ public:
         //     }
         // }
         // return ans;
-        for(int i=n-1;i>=0;i--){
-            while(!st.empty() && t[st.top()]<=t[i]){
+
+        //optmised approach
+        for(int i=n-1;i>=0;i--){//moving from right to left
+            while(!st.empty() && t[st.top()]<=t[i]){//till the top element is less than the current element pop the top 
                 st.pop();
             }
-            if(!st.empty()){
-                ans[i]=st.top()-i;
+            //here we didnt add s=any empty condition as in problem we have been given that for the temp if there is no highest on right side let it remain 0
+            if(!st.empty()){//now when popping is done
+            //which means the ith element is smaller than the top element 
+                ans[i]=st.top()-i;//so we will subtract the indexes to get the count
             }
-            st.push(i);
+            st.push(i);//then again push the current element onto the stack 
         }
         return ans;
     }
