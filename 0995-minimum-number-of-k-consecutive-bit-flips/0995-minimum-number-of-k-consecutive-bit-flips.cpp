7a5f3fda5aff42280db799dbbb7a1a5d57043572
bhavesharora <143539122+bhavesh6119@users.noreply.github.com>
2026-07-16 19:46:04 +0530

    Time: 9 ms (28.96%), Space: 112.2 MB (52.98%) - LeetHub

--- 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.cpp
+++ 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.cpp
@@ -21,18 +21,23 @@ public:
         int n=nums.size();
         int flips=0;
         queue<int>q;
-        for(int i=0;i<n;i++){
-            if(!q.empty() && q.front()<i){
-                q.pop();
+        for(int i=0;i<n;i++){//iterate on the nums array
+            if(!q.empty() && q.front()<i){//if q is not empty and front is less than current index
+                q.pop();//pop it
             }
-            if(q.size()%2==nums[i]){
-                if(i+k-1>=n){
+            if(q.size()%2==nums[i]){//otherwise if this condition is satisfied then we have to flip
+                if(i+k-1>=n){//if exceeds the window size
                     return -1;
                 }else{
-                    q.push(i+k-1);
-                    flips++;
+                    q.push(i+k-1);//push the last index of the window
+                    flips++;//increase the flips
                 }
             }
+            //here size of q determines that how many flips have been made until the current index 
+            //and if the size is odd and element is also 1 then we have to flip
+            //if the size is even and element is also 0 then we have to flip 
+            //as odd%2==1 -> 1 flipped odd times turn 0 so here we flip
+            //similarly even%2==0 -> 0 flipped even times turn 0 so here we flip again
         }
         return flips;
     }
