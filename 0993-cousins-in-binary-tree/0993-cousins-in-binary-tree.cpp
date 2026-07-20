5a1ebfbf30887900b7447c6b644914d7a8839a40
bhavesharora <143539122+bhavesh6119@users.noreply.github.com>
2026-07-20 19:09:30 +0530

    Time: 0 ms (100%), Space: 14.1 MB (48.49%) - LeetHub

--- 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.cpp
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.cpp
@@ -15,15 +15,15 @@ public:
         if(root==NULL){
             return 0;
         }
-        if(root->left && root->right){
-        if(root->left->val==x && root->right->val==y){
+        if(root->left && root->right){//if both right and left exist
+        if(root->left->val==x && root->right->val==y){//check if any parent has right and left value same as given 
             return 1;
         }
-        if(root->left->val==y && root->right->val==x){
+        if(root->left->val==y && root->right->val==x){//same here
             return 1;
         }
         }
-        return parents(root->left,x,y) || parents(root->right,x,y);
+        return parents(root->left,x,y) || parents(root->right,x,y);//otherwise return the or of the left and right
     }
     bool isCousins(TreeNode* root, int x, int y) {
         int level=0;
@@ -32,31 +32,31 @@ public:
         queue<TreeNode*>q;
         q.push(root);
         while(!q.empty()){
-            int n=q.size();
+            int n=q.size();//storing the size of current q
             while(n--){
-                TreeNode *temp=q.front();
+                TreeNode *temp=q.front();//storing the front node address
                 q.pop();
-                if(temp->val==x){
-                    l1=level;
+                if(temp->val==x){//now checking if the val matches the given node or not
+                    l1=level;//mark the level
                 }
-                if(temp->val==y){
+                if(temp->val==y){//same here
                     l2=level;
                 }
-                if(temp->left){
+                if(temp->left){//normal queue level wise traversal
                     q.push(temp->left);
                 }
                 if(temp->right){
                     q.push(temp->right);
                 }
             }
-            level++;
+            level++;//increse the level after completely iterating the size of q
             if(l1!=l2){
-                return 0;
+                return 0;//if they are not on equal levels
             }
-            if(l1!=-1 || l2!=-1){
+            if(l1!=-1 || l2!=-1){//if they have got their level assigned 
                 break;
             }
         }
-        return !parents(root,x,y);
+        return !parents(root,x,y);//return inverse of this function
     }
 };
