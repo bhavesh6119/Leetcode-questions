bfb6d28d8f589c2c5a8c45fa1da255f23244ffc7
bhavesharora <143539122+bhavesh6119@users.noreply.github.com>
2026-07-22 15:07:33 +0530

    Time: 0 ms (100%), Space: 14.9 MB (63.95%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -11,28 +11,43 @@
  */
 class Solution {
 public:
-    vector<int> rightSideView(TreeNode* root) {
-        //using queue
-        queue<TreeNode*>q;
-        q.push(root);
-        vector<int>ans;
+    void rightview(TreeNode* root,int level,vector<int>&ans){
         if(!root){
-            return ans;
+            return;
         }
-        while(!q.empty()){
-            int n=q.size();
-            ans.push_back(q.front()->val);//pushing the first value seen at a particular level
-            while(n--){
-                TreeNode*temp=q.front();//storing it in temp
-                q.pop();//popping it
-                if(temp->right){//then pushing its right to queue first
-                    q.push(temp->right);
-                }
-                if(temp->left){//then left
-                    q.push(temp->left);
-                }
-            }
+        if(level==ans.size()){
+            ans.push_back(root->val);
         }
+        rightview(root->right,level+1,ans);
+        rightview(root->left,level+1,ans);
+    }
+    vector<int> rightSideView(TreeNode* root) {
+        // //using queue
+        // queue<TreeNode*>q;
+        // q.push(root);
+        // vector<int>ans;
+        // if(!root){
+        //     return ans;
+        // }
+        // while(!q.empty()){
+        //     int n=q.size();
+        //     ans.push_back(q.front()->val);//pushing the first value seen at a particular level
+        //     while(n--){
+        //         TreeNode*temp=q.front();//storing it in temp
+        //         q.pop();//popping it
+        //         if(temp->right){//then pushing its right to queue first
+        //             q.push(temp->right);
+        //         }
+        //         if(temp->left){//then left
+        //             q.push(temp->left);
+        //         }
+        //     }
+        // }
+        // return ans;
+        
+        //using recursion now
+        vector<int>ans;
+        rightview(root,0,ans);
         return ans;
     }
 };
