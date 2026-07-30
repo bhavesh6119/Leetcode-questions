39bb6906a2a65562a62667629f6c2627c7ad601d
bhavesharora <143539122+bhavesh6119@users.noreply.github.com>
2026-07-30 12:46:54 +0530

    Time: 0 ms (100%), Space: 13.7 MB (83.79%) - LeetHub

--- 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.cpp
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.cpp
@@ -11,23 +11,44 @@
  */
 class Solution {
 public:
-    bool isCompleteTree(TreeNode* root) {
-        queue<TreeNode*>q;
-        q.push(root);
-        bool past=false;
-        while(!q.empty()){
-            TreeNode* temp=q.front();
-            q.pop();
-            if(temp==NULL){
-                past=true;
-            }else{
-                if(past==true){
-                    return false;
-                }
-                q.push(temp->left);
-                q.push(temp->right);
-            }
+    int count(TreeNode*root){
+        if(!root){
+            return 0;
         }
-        return true;
+        return 1+count(root->left)+count(root->right);
+    }
+    bool dfs(TreeNode*root,int i,int total){
+        if(!root){
+            return true;
+        }
+        if(i>total){
+            return false;
+        }
+        return dfs(root->left,2*i,total) && dfs(root->right,2*i+1,total);
+    }
+    bool isCompleteTree(TreeNode* root) {
+        // //BFS
+        // queue<TreeNode*>q;
+        // q.push(root);
+        // bool past=false;
+        // while(!q.empty()){
+        //     TreeNode* temp=q.front();
+        //     q.pop();
+        //     if(temp==NULL){
+        //         past=true;
+        //     }else{
+        //         if(past==true){
+        //             return false;
+        //         }
+        //         q.push(temp->left);
+        //         q.push(temp->right);
+        //     }
+        // }
+        // return true;
+
+        //DFS
+        int TotalNodes=count(root);
+        int i=1;
+        return dfs(root,i,TotalNodes);
     }
 };
