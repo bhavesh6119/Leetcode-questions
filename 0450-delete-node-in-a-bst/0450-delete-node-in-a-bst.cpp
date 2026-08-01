7be8c37d41a3a015c71b315b75db09f612d33e13
bhavesharora <143539122+bhavesh6119@users.noreply.github.com>
2026-08-01 18:40:11 +0530

    Time: 0 ms (100%), Space: 34.3 MB (88.19%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -12,48 +12,48 @@
 class Solution {
 public:
     TreeNode* deleteNode(TreeNode* root, int key) {
-        if(!root){
+        if(!root){//base case
             return NULL;
         }
-        if(root->val > key){
+        if(root->val > key){//got to left
             root->left=deleteNode(root->left,key);
             return root;
         }
-        if(root->val < key){
+        if(root->val < key){//go to right
             root->right=deleteNode(root->right,key);
         }
-        else{
-            if(!root->left && !root->right){
+        else{//if target is found
+            if(!root->left && !root->right){//if it is a leaf node
                 delete root;
                 return NULL;
-            }else if(!root->right){
+            }else if(!root->right){//if it left exist
                 TreeNode*temp=root->left;
                 delete root;
                 return temp;
-            }else if(!root->left){
+            }else if(!root->left){//if its right exist
                 TreeNode*temp=root->right;
                 delete root;
                 return temp;
-            }else{
-                TreeNode*child=root->left;
-                TreeNode*parent=root;
-                while(child->right){
+            }else{//if left and right both exist
+                TreeNode*child=root->left;//go to left side
+                TreeNode*parent=root;//make it root
+                while(child->right){//go to extreme right to get greater element
                     parent=child;
                     child=child->right;
                 }
-                if(root!=parent){
+                if(root!=parent){//if the right is present 
                     parent->right=child->left;
                     child->left=root->left;
                     child->right=root->right;
                     delete root;
                     return child;
-                }else{
+                }else{//if there is no right 
                     child->right=root->right;
                     delete root;
                     return child;
                 }
             }
         }
-        return root;
+        return root;//return the root 
     }
 };
