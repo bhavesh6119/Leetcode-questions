5c5d42013f6a7414d0ec6ed4d0ed140317f7fef3
bhavesharora <143539122+bhavesh6119@users.noreply.github.com>
2026-08-12 11:07:59 +0530

    Time: 62 ms (65.95%), Space: 149.4 MB (56.74%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -2,21 +2,22 @@ class Solution {
 public:
     int maxSubarrayLength(vector<int>& nums, int k) {
         int n=nums.size();
+        //using two pointer approach
         int left=0;
         int right=0;
         int ans=0;
-        unordered_map<int,int>mp;
-        while(right<n){
-            mp[nums[right]]++;
-            if(mp[nums[right]]<=k){
-                ans=max(ans,right-left+1);
+        unordered_map<int,int>mp;//storing the frequency of the elements
+        while(right<n){//till right!=sizeof array
+            mp[nums[right]]++;//increment freq
+            if(mp[nums[right]]<=k){//if freq is less than or equal to k
+                ans=max(ans,right-left+1);//update the ans
             }else{
-               while(mp[nums[right]]>k){ 
-                mp[nums[left]]--;
-                left++;
+               while(mp[nums[right]]>k){ //if at any point it becomes greater
+                mp[nums[left]]--;//ruduce the freq of the first element of subarray 
+                left++;//reduce subarray size
                }
             }
-            right++;
+            right++;//otherwise increase the subarray size
         }
         return ans;
     }
