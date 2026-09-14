31bfdf23a6d01dbe9791cc3f6c9546b8126c7c09
bhavesharora <143539122+bhavesh6119@users.noreply.github.com>
2026-09-14 14:13:35 +0530

    Time: 7 ms (11.73%), Space: 19.6 MB (59.38%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,28 +1,46 @@
 class Solution {
 public:
-    int findCircleNum(vector<vector<int>>& adj) {
-        int n=adj.size();
-        queue<int>q;
-        vector<bool>visited(n,0);
+    int findparent(int node, vector<int>&parent){
+        if(node==parent[node]){
+            return node;
+        }
+
+        parent[node]=findparent(parent[node],parent);
+        return parent[node];
+    }
+
+    void unionbyrank(int u,int v,vector<int>&parent,vector<int>&rank){
+        int pu=findparent(u,parent);
+        int pv=findparent(v,parent);
+
+        if(rank[pu]>rank[pv]){
+            parent[pv]=pu;
+        }else if(rank[pv]>rank[pu]){
+            parent[pu]=pv;
+        }else{
+            parent[pu]=pv;
+            rank[pv]++;
+        }
+    }
+    int findCircleNum(vector<vector<int>>& isconnected) {
+        int n=isconnected.size();
+        int m=isconnected[0].size();
+        vector<int>parent(n);
+        vector<int>rank(n,0);
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(isconnected[i][j]==1){
+                    unionbyrank(i,j,parent,rank);
+                }
+            }
+        }
         int count=0;
         for(int i=0;i<n;i++){
-            if(!visited[i]){
+            if(findparent(i,parent)==i){
                 count++;
-            }
-            visited[i]=1;
-            q.push(i);
-            while(!q.empty()){
-                int node=q.front();
-                q.pop();
-                for(int j=0;j<n;j++){
-                    if(visited[j]==1){
-                        continue;
-                    }
-                    if(adj[node][j]==1 && !visited[j]){
-                        q.push(j);
-                        visited[j]=1;
-                    }
-                }
             }
         }
         return count;
