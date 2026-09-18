fdaf5974075078b9d4695e686eadc5022e828f6e
bhavesharora <143539122+bhavesh6119@users.noreply.github.com>
2026-09-18 17:51:44 +0530

    Time: 234 ms (80.24%), Space: 8.8 MB (87.57%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,51 +1,51 @@
 class Solution {
 public:
     bool check(int num,vector<vector<char>>&board,int i,int j){
-        char c=num+'0';
-        for(int col=0;col<9;col++){
+        char c=num+'0';//take the number char
+        for(int col=0;col<9;col++){//find in the curr col
             if(board[i][col]==c){
-                return 0;
+                return 0;//if exist return 0
             }
         }
-        for(int row=0;row<9;row++){
+        for(int row=0;row<9;row++){//find in the curr row
             if(board[row][j]==c){
                 return 0;
             }
         }
-        int row=i/3*3;
-        int col=j/3*3;
-        for(int a=row;a<row+3;a++){
+        int row=i/3*3;//to find the start of the box in rows
+        int col=j/3*3;//to find the start of the box in col
+        for(int a=row;a<row+3;a++){//iterate
             for(int b=col;b<col+3;b++){
-                if(board[a][b]==c){
+                if(board[a][b]==c){//if found return 0
                     return 0;
                 }
             }
         }
-        return 1;
+        return 1;//if not found anywhere
     }
     bool find(int i,int j,vector<vector<char>>&board){
-        if(i==9){
+        if(i==9){//base condition
             return 1;
         }
-        if(j==9){
+        if(j==9){//if we reach the last col then proceed to next row 
             return find(i+1,0,board);
         }
-        if(board[i][j]!='.'){
+        if(board[i][j]!='.'){//if we dont find the empty cell in the current column move to next column
             return find(i,j+1,board);
         }
 
-        for(int num=1;num<10;num++){
-            if(check(num,board,i,j)){
-                board[i][j]='0'+num;
-                if(find(i,j+1,board)){
-                    return 1;
+        for(int num=1;num<10;num++){//iterate on the numbers
+            if(check(num,board,i,j)){//here we check for the conditions
+                board[i][j]='0'+num;//if condition in true then place the number char
+                if(find(i,j+1,board)){//go to next column 
+                    return 1;//if it return true that means the number has been placed correctly
                 }
-                board[i][j]='.';
+                board[i][j]='.';//otherwise backtrack make it empty again
             }
         }
-        return 0;
+        return 0;//if not properly placed
     }
     void solveSudoku(vector<vector<char>>& board) {
-        find(0,0,board);
+        find(0,0,board);//just pass onto function
     }
 };
